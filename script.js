2c1fc43a452e3d51116f1be6e41a1607cef7e65a
William Burroughs <willcburroughs@gmail.com>
2024-04-16 18:20:28 -0400

    Adding header element

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ testElement.innerText = "Hello There Friend!"
 testElement.classList.add("makeRed")
 
 document.body.appendChild(testElement);
+
+let blueElement = document.createElement("h3")
+blueElement.innerText = "I’m a blue h3!"
+
+blueElement.classList.add("makeBlue")
+
+document.body.appendChild(blueElement);
+
+
+
+
